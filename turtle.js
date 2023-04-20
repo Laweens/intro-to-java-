b3be33f48853f1fee509cbb72854d695cfd0aeda
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-04-20 14:03:08 -0400

    finished house

--- turtle.js
+++ turtle.js
@@ -1,68 +1,100 @@
 let aquarium = document.getElementById("aquarium")
 let turtle = new RealTurtle.default(aquarium, {autoStart: true})
 
-
-turtle.setStrokeColorRGB(225, 00, 0)
-turtle.setFillStyle('red')
+turtle.setSpeed(0.95)
+turtle.setPosition(0,0)
+turtle.setFillStyle('skyblue')
 turtle.beginPath()
-
-
-
-
+for (let i =0; i < 4; i++)
+{
+    turtle.right(90)
+    turtle.forward(300)
+}
 turtle.closePath()
 turtle.fill()
 
+
+
+
 turtle.setPosition(40, 150)
+turtle.beginPath()
 
  for ( let i = 0; i < 4; i++)
 {
     turtle.forward(80)
     turtle.right(90)
 }
-turtle.beginPath()
 
-turtle.fill('red')
-turtle.setFillStyle('red')
+turtle.setStrokeColorRGB(225, 00, 0)
+turtle.setFillStyle('blue')
+
+
+
+
+
 turtle.closePath()
 
+
+turtle.fill('red')  
+
 turtle.setPosition(69, 150)
+turtle.beginPath()
 for ( let i = 0; i < 4; i++)
 {
     turtle.forward(20)
     turtle.right(90)  
 }
+turtle.setStrokeColorRGB(225, 00, 0)
+turtle.setFillStyle('white')
+
+
+
+
+
+turtle.closePath()
+turtle.fill('grey')  
+
+
 turtle.setPosition(40,70)
+turtle.beginPath()
 for ( let i = 0; i < 2; i++)
 {
-    turtle.arc(0, 45)
+    turtle.right(45)
     turtle.forward(69.5)
     turtle.right(90)  
-    turtle.arc(0, -31)
+    turtle.left(31)
 
 }
+turtle.setStrokeColorRGB(225, 00, 0)
+turtle.setFillStyle('grey')
+
+
+
+
+
+turtle.closePath()
+turtle.fill()  
+
 
 turtle.setPosition(65,100)
 turtle.arc(0,64)
+
  for ( let i = 0; i < 4; i++)
  {
      turtle.forward(15)
      turtle.right(90)
  }
- turtle.beginPath()
- turtle.fill('red')
- turtle.setFillStyle('red')
- turtle.closePath()
+ 
+
  
 
  turtle.setPosition(105,100)
+
  for ( let i = 0; i < 4; i++)
  {
      turtle.forward(15)
      turtle.right(90)
  }
- turtle.beginPath()
- turtle.setFillStyle('red')
- turtle.fill('red')
- turtle.closePath()
- 
+
+
  
