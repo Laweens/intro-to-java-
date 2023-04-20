6702ffd3b2214583aa4efb7599aa783fc7665cb6
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-04-20 14:53:15 -0400

    polygon

---
+++ turtle2.js
@@ -0,0 +1,17 @@
+let aquarium = document.getElementById("aquarium")
+let turtle = new RealTurtle.default(aquarium, {autoStart: true})
+
+turtle.setSpeed(0.99)
+
+function polygon(sides, size = 20)
+{
+    for (let i=0; i < sides; i++) {
+        turtle.forward(size)
+        turtle.right(360/sides)
+    }
+}
+
+for(let i = 0; i <5; i++ ) {
+    polygon((Math.random() * 10 ) + 3 )
+
+}
