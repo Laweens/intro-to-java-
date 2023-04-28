b2484e1e3050598fc405306d0d3ce04430cf9f49
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-04-28 14:52:14 -0400

    stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,14 @@
+
+
+function start () {
+   alert(" you clicked the start button") 
+}
+   
+function stop () {
+    alert(" you clicked the stop button") 
+ }
+   
+ function restart () {
+    alert(" you've restarted the stop watch") 
+ }
+    
