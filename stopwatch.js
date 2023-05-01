8859e9f7f292c95746e9ec9c5045563212095aea
Laweens Francois <lfrancois@brooklynsteamcenter.org>
2023-05-01 14:51:44 -0400

    button optimization

--- stopwatch.js
+++ stopwatch.js
@@ -1,14 +1,60 @@
+let seconds= 0
+let milliseconds= 0
+let minutes = 0
 
+let digits = document.getElementById('digits')
+
+let interval;
 
 function start () {
-   alert(" you clicked the start button") 
+ 
+    restart()
+
+
+   interval = setInterval (function () {
+milliseconds = milliseconds + 10
+  
+ if (milliseconds >= 1000) {
+    seconds = seconds + 1
+    milliseconds = 0
+ }
+ if (seconds >= 60) {
+    minutes = minutes + 1
+    seconds = 0
+ }
+
+ console.log(milliseconds)
+ console.log(seconds)
+ console.log(minutes)
+
+digits.innerHTML= minutes + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds
+
+
+
+}, 10)
 }
    
 function stop () {
-    alert(" you clicked the stop button") 
+    
+
+      clearInterval(interval)
+      digits.innerHTML= minutes + ':' + String(seconds).padStart(2, '0') + ':' + String(milliseconds).padStart(2, '0')
+
+
+
  }
    
  function restart () {
-    alert(" you've restarted the stop watch") 
+    
+
+    clearInterval(interval)
+    seconds = 0
+    milliseconds = 0
+    minutes = 0
+    digits.innerHTML= minutes + ':' + String(seconds).padStart(2, '0') + ':' + String(milliseconds).padStart(2, '0')
+
+
+
+
  }
     
